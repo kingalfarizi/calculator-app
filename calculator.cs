b4d11f5fb22ff474f36f0ac63114020ac474da31
kingalfarizi <alfarizidito@gmail.com>
2021-03-09 15:01:14 +0700

    Melengkapi Fungsi Main

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,13 @@ class Calculator
 {
 	static void main(string[] args)
 	{
-	
+		int a = 10;
+		git b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, pengurangan(a,b));
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	
 	public int penambahan(int a, int b)
